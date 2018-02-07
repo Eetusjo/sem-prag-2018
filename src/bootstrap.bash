a92ed096cbf70e3cc168276939808f95d5ff1753
Eetusjo <eetu.sjoblom@helsinki.fi>
2018-02-07 15:29:25 +0200

    new bootstrap script with nltk installations

--- src/bootstrap.bash
+++ src/bootstrap.bash
@@ -8,3 +8,8 @@ export GIT_COMMITTER_NAME=anonymous
 export GIT_COMMITTER_EMAIL=anon@localhost
 git clone https://github.com/Eetusjo/sem-prag-2018.git
 
+pip install nltk
+python -c "import nltk; nltk.download( \
+["gutenberg", "genesis", \
+"treebank", "nps_chat", \
+"inaugural", "webtext"])"
